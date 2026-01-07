47bd0b9c71833aef4a19827803754dd323c65891
NIPUNASUDESH <nipunasudesh2000@gmail.com>
2026-01-07 15:28:14 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen flex items-center justify-center bg-blue-100">
+      <h1 className="text-3xl font-bold text-blue-700">
+        Tailwind CSS is working!
+      </h1>
+    </div>
     </div>
   );
 }
