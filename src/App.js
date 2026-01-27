2ac81f074e8daba11ba932cb0b6c9635a051ea8c
NIPUNASUDESH <nipunasudesh2000@gmail.com>
2026-01-27 10:22:26 +0530

    change file path

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import PublicLayout from "./components/layouts/PublicLayout";
-import HeartCursor from "./components/layouts/HeartCursor";
+import PublicLayout from "./components/layout/PublicLayout";
+import HeartCursor from "./components/layout/HeartCursor";
 import Home from "./Pages/home/Home";
 import AllCategories from "./Pages/AllCategories";
 import CategoryPage from "./Pages/singleCategory/CategoryPage";
