540af76e0140a26d7ba181e815b4ffdab3f9fc97
NIPUNASUDESH <nipunasudesh2000@gmail.com>
2026-05-08 14:28:44 +0530

    add menu

--- src/Pages/AdminPanel/CategoriesManagement.jsx
+++ src/Pages/AdminPanel/CategoriesManagement.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import AdminLayout from './AdminLayout'
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Menu } from "lucide-react";
 export const CategoriesManagement = () => {
   return (
     <AdminLayout>
